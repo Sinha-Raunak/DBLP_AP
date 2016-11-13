baefb819e216ca081ecbb045f7af03607c062f18
Ishbir Walia <Ishbir Walia>
2016-11-13 21:59:33 +0530

    AP Code first commit

---
+++ Codes/Article.java
@@ -0,0 +1,72 @@
+
+public class Article {
+	private String author;
+	private String title;
+	private String pages;
+	private String year;
+	private String volume;
+	private String journal;
+	private String number;
+	private String url;
+	private String ee;
+	
+	Article()
+	{
+		author=new String();
+		title=new String();
+		pages=new String();
+		year=new String();
+		volume=new String();
+		journal=new String();
+		number=new String();
+		url=new String();
+		ee=new String();
+		
+	}
+	
+	
+	public void setAuthor(String str)
+	{
+		author=new String(str);
+	}
+	
+	public void setTitle(String str)
+	{
+		title=new String(str);
+	}
+	
+	public void setPages(String str)
+	{
+		pages=new String(str);
+	}
+	
+	public void setYear(String str)
+	{
+		year=new String(str);
+	}
+	
+	public void setVolume(String str)
+	{
+		volume=new String(str);
+	}
+	
+	public void setJournal(String str)
+	{
+		journal=new String(str);
+	}
+	
+	public void setNumber(String str)
+	{
+		number=new String(str);
+	}
+	
+	public void setUrl(String str)
+	{
+		url=new String(str);
+	}
+	
+	public void setEE(String str)
+	{
+		ee=new String(str);
+	}
+}
